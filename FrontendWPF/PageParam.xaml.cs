b837fe07843b8bb1f7f76ec29746f1400a99c1ff
HANA BERRACHED <hana.berrached@viacesi.fr>
2023-12-11 16:57:08 +0100

    WPF PART

--- FrontendWPF/PageParam.xaml.cs
+++ FrontendWPF/PageParam.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,18 +16,13 @@ using System.Windows.Shapes;
 namespace FrontendWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour PageParam.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PageParam : Page
     {
-        public MainWindow()
+        public PageParam()
         {
             InitializeComponent();
-        }
-
-        private void InitializeComponent()
-        {
-            throw new NotImplementedException();
         }
     }
 }
